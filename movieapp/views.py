372381d4ee8c501c024c8ea827af6a4ceee51fee
Clins-Thomas <139744083+Clins-Thomas@users.noreply.github.com>
2023-09-06 13:59:06 +0530

    my fav movie

---
+++ movieapp/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import movieform
+from.models import movie
+
+# Create your views here.
+def index(request):
+    Movie=movie.objects.all()
+    context={
+        'movie_list':Movie
+    }
+    return render(request,'index.html',context)
+def details(request,movie_id):
+    Movie=movie.objects.get(id=movie_id)
+    return render(request,"details.html",{'movie':Movie})
+def add_movie(request):
+    if request.method=="POST":
+        name=request.POST.get('name',)
+        desc = request.POST.get('desc', )
+        year = request.POST.get('year', )
+        img = request.FILES['img']
+        Movie=movie(name=name,desc=desc,year=year,img=img)
+        Movie.save()
+    return render (request,'add.html')
+def update(request,id):
+    Movie=movie.objects.get(id=id)
+    form=movieform(request.POST or None, request.FILES,instance=Movie)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'form':form,'movie':Movie})
+def delete(request,id):
+    if request.method=='POST':
+        Movie=movie.objects.get(id=id)
+        Movie.delete()
+        return redirect('/')
+    return render(request,'delete.html')
